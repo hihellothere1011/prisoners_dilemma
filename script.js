0adcc6a7190e7f47729375debacf252fbd5f8a4b
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-08-30 21:06:56 +0800

    organized

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /*picture handling*/
+const picture = {name: warden, value:"https://raw.githubusercontent.com/hihellothere1011/prisoners_dilemma/main/images/%E5%B0%88%E6%A1%88.png"}
+const warden = document.getElementById("warden")
 
-
+document.warden.style.picture = picture[warden]
 //code
 
 
