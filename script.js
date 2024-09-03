390a9e491ffa8cec75bd859b694fb476f807d14b
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-09-03 20:50:32 +0800

    organized

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 /*picture handling*/
-const picture = {name: warden, value:"https://raw.githubusercontent.com/hihellothere1011/prisoners_dilemma/main/images/%E5%B0%88%E6%A1%88.png"}
+const picture = {
+    name: npc, value:""
+}
 const warden = document.getElementById("warden")
 
-warden.style.picture = picture[warden]
+warden.style.backgroundImage = picture[warden]
 //code
 
 
 
 /*button handling */
-const blacksheet = document.getElementById("blacksheet")
-function disappear() {
-    blacksheet.style.display= "none"
-}
-blacksheet.addEventListener("click", disappear)
+const blacksheet = document.getElementById("intro")
+blacksheet.addEventListener("click", blacksheet.style.display = "none")
 
 //code
 
