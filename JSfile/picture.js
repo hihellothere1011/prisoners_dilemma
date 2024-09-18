69055d650aa4528a32d3686aa0b829ad6a4be5d9
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-09-18 22:12:52 +0800

    games made

--- JSfile/picture.js
+++ JSfile/picture.js
@@ -1,4 +1,5 @@
 /*picture handling*/
 const picture = {
     name: player2, value:"hello"
+    
 }
--- JSfile/script.js
+++ JSfile/script.js
@@ -6,22 +6,36 @@ import player from "./player.js"
 betray()
 cooperate()
 
+
 let player1 = playerCreation("Austin", 0)
-let p1M = document.getElementById("p1")
-let player2 = playerCreation("Bob", 10)
-let p2M = document.getElementById("p2")
+let player2 = playerCreation("Bob", 0)
+
 
 function playerCreation(name, coins) {
     const plays = new player(name, coins)
     return plays
 }
 
+function startTheGame() {
+    document.getElementById("mainPart").style.display = "block"
+    
+}
+
 
 
 function nameSetting() {
-    let p1nameSub
+    document.getElementById("play").addEventListener("click", (event) => {
+        event.preventDefault()
+        startTheGame()
+        let playerName = document.getElementById("p1name").value
+        player1.name = playerName
+        document.getElementById("playername").textContent = player1.name
+        document.getElementById("player2name").textContent = player2.name
+        console.log(player1.name)
+    })
 }
 
+nameSetting()
 
 function generateDecision() {
     return Math.random() < 0.5 ? "betray" : "cooperate";
@@ -31,6 +45,8 @@ function generateDecision() {
 
 /*point system */
 function pointSystem(p1Decision, p2Decision = generateDecision()) {
+    let p1M = document.getElementById("p1")
+    let p2M = document.getElementById("p2")
     console.log(p1Decision)
     console.log(`${p2Decision}-random`)
     switch (`${p1Decision}-${p2Decision}`) {
@@ -39,28 +55,24 @@ function pointSystem(p1Decision, p2Decision = generateDecision()) {
             player2.money += 1
             p1M.textContent = player1.money
             p2M.textContent = player2.money
-            console.log(player1.money, p1Decision, player2.money, p2Decision)
             break;
         case "betray-cooperate":
             player1.money += 5
             player2.money += 0
             p1M.textContent = player1.money
             p2M.textContent = player2.money
-            console.log(player1.money, p1Decision, player2.money, p2Decision)
             break;
         case "cooperate-betray":
             player1.money += 0
             player2.money += 5
             p1M.textContent = player1.money
             p2M.textContent = player2.money
-            console.log(player1.money, p1Decision, player2.money, p2Decision)
             break;
         case "cooperate-cooperate":
             player1.money += 3
             player2.money += 3
             p1M.textContent = player1.money
             p2M.textContent = player2.money
-            console.log(player1.money, p1Decision, player2.money, p2Decision)
             break;
         default:
             console.log("Invalid decisions");
