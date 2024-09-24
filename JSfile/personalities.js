7a321af8ec16b3d59805488897542f7e5f786dc6
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-09-24 22:09:59 +0800

    personality system

--- JSfile/personalities.js
+++ JSfile/personalities.js
@@ -1,12 +1,26 @@
+function zeroAndOne() {
+    return Math.floor(Math.random() * 2)
+}
+
 export default function generateDecision(player) {
     switch (player.personality) {
-        case 0:
-            console.log("deciding...")    
+        case 0 /*Randomly */:   
             return Math.random() < 0.5 ? "betray" : "cooperate";
-        case 1:
-            if (!player.history[0]) {
+        case 1 /*Tit for tat */:
+            if (!player.history.length) {
                 return "cooperate"
+            } else {
+                return player.history[player.history.length - 1][0]
             }
-            console.log(`p2's decision: ${player.history[0][0]}`)   
+        case 2 /*Kindness */:
+            if (!player.history.length) {
+                return "betray"
+            } else if (player.history[player.history.length - 1][0] === "cooperate") {
+                return "cooperate"
+            } else if (zeroAndOne() === 0) {
+                return "cooperate"
+            } else {
+                return "betray"
+            }
     }
 }
--- JSfile/script.js
+++ JSfile/script.js
@@ -9,11 +9,9 @@ cooperate()
 
 
 let player1 = playerCreation("Austin", 0)
-let player2 = playerCreation("Bob", 0, 1)
+let player2 = playerCreation("Bob", 0)
 let games = 0
 let roundsLeft = numOfRound(10,20)
-
-console.log(player2)
 
 function playerCreation(name, money, personality=0) {
     const plays = new player(name, money, personality)
@@ -27,9 +25,13 @@ function numOfRound(min=10, max=15) {
 
 function startTheGame() {
     document.getElementById("mainPart").style.display = "block"
+    document.getElementById("coinsTotal1").style.display = "none"
+    document.getElementById("coinsTotal2").style.display = "none"
     games += 1
     document.getElementById("games").textContent = `GAME: ${games}`
+    player2.personality = Math.floor(Math.random() * 3)
 
+    console.log(`P2 personality: ${player2.personality}`)
 }
 
 
@@ -42,7 +44,7 @@ function nameSetting() {
         player1.name = playerName
         document.getElementById("playername").textContent = player1.name
         document.getElementById("player2name").textContent = player2.name
-        console.log(player1.name)
+        document.getElementById("text").style.display = "none"
     })
 }
 
@@ -86,24 +88,33 @@ function pointSystem(p1Decision, p2Decision = generateDecision(player2)) {
         
     }
     player2.history.push([p1Decision,p2Decision])
-    console.log(player2.history)
 }
 
-/* blacksheet handling */
-const blacksheet = document.getElementById("intro")
-blacksheet.addEventListener("click", () => {
-    blacksheet.style.display = "none"
-})
-
-
+function gameEnds() {
+    console.log("Game ends")
+    let p1M = document.getElementById("p1")
+    let p2M = document.getElementById("p2")
+    p1M.textContent = 0
+    p2M.textContent = 0
+    roundsLeft = numOfRound(10,20)
+    document.getElementById("coinsTotal1").textContent = `P1 Coins in total: ${player1.money}`
+    document.getElementById("coinsTotal2").textContent = `P2 Coins in total: ${player2.money}`
+    document.getElementById("coinsTotal1").style.display = "flex"
+    document.getElementById("coinsTotal2").style.display = "flex"
+    player1.money = 0
+    player2.money = 0
+    player1.history = []
+    player2.history = []
+    document.getElementById("text").style.display = "flex"
+    document.getElementById("mainPart").style.display = "none"
+}
 
 /* betray & cooperate handling */
 function buttonPress(p1,p2) {
     roundsLeft -= 1
     console.log(roundsLeft)
     if (roundsLeft <= 0) {
-        console.log("Game ends")
-        document.getElementById("mainPart").style.display = "none"
+        gameEnds()
     }
 }
 
